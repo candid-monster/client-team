6c0f33bb76ef10cb87c5cf6758a162e0fe96cac3
20192094 <52098309+20192094@users.noreply.github.com>
2023-08-29 14:13:42 +0900

    feat: add item for test inventory

--- Assets/Scripts/Item/UsableItem.cs
+++ Assets/Scripts/Item/UsableItem.cs
@@ -2,14 +2,13 @@
 {
     public class UsableItem : Item
     {
-
         public UsableItem(ElementalStatus s) : base(s)
         {
         }
 
         public void Use(Player target)
         {
-            
+
         }
     }
 }
